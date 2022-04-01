791b446bb17b7a7b1d2f894b246d2642dbfa6dbe
Iqra <iqrachoudry92@gmail.com>
2022-03-31 21:10:07 -0400

    Commit on Mind Benders

--- src/test/java/com/amazonPageElements/AmazonEndtoEndTestingElements.java
+++ src/test/java/com/amazonPageElements/AmazonEndtoEndTestingElements.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
  
-
+ 
 public class AmazonEndtoEndTestingElements {
 
 		@FindBy(id = "ap_email")
